f7c9429ce689f2233dc3abe47b2d5940c9e32e71
Batubo Victory <victorybatubo76@gmail.com>
2026-02-04 18:00:17 +0100

    style: Add all color styles for different variants

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,14 +4,15 @@ import type { ButtonVariant } from '../../types/UiTypes';
 function Button({label, type, variant}: ButtonProps){
 
     const variants: ButtonVariant = {
-        primary: 'bg-blue-600 hover:bg-blue-400 shadow-blue-400',
-        secondary: '',
+        primary: 'bg-blue-600 hover:bg-blue-400 shadow-blue-400 shadow-2xl text-white',
+        secondary: 'bg-blue-50 border-2 hover:bg-blue-200 text-blue-500 font-semibold',
+        danger: 'bg-red-600 hover:bg-red-400 shadow-red-400 text-white'
     }
 
     return(
         <button
             type={type}
-            className={`px-6 py-3 text-white rounded-full transition-all ease-in duration-200 shadow-2xl ${variants[variant]}`}
+            className={`px-6 py-3 rounded-full transition-all ease-in duration-200 ${variants[variant]}`}
         >
             {label}
         </button>
