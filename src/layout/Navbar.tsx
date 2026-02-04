7a1eaf88d505af1d4d84d3682841b0cffe4e8998
Batubo Victory <victorybatubo76@gmail.com>
2026-02-04 16:10:06 +0100

    feat: Add logo to navbar

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import Logo from '../../public/Images/Logo/ClearListing_Logo.png'
 function Navbar(){
     return(
         <nav>
-            <h3>ClearEstate</h3>
+            <img src={Logo} alt="ClearListing Logo" />
         </nav>
     )
 }
