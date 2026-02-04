2c1c4086f0ac490ecdb257f42a15624c79d20b69
Batubo Victory <victorybatubo76@gmail.com>
2026-02-04 17:15:50 +0100

    feat: Create styles for Button Variants

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,10 @@
 import type { ButtonProps } from '../../types/UiTypes';
-import type { ButtonVariants } from '../../types/UiTypes';
+import type { ButtonVariant } from '../../types/UiTypes';
 
 function Button({label, type, variant}: ButtonProps){
 
-    const variants: ButtonVariants = {
-        primary: 'text-white',
+    const variants: ButtonVariant = {
+        primary: 'px-6 py-3 bg-blue-600 text-white rounded-full hover:bg-blue-400 transition-all ease-in duration-200 shadow-2xl shadow-blue-400',
         secondary: '',
     }
 
