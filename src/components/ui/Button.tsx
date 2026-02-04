2ac4b3dd715f2010029cf1de20020aad85c53965
Batubo Victory <victorybatubo76@gmail.com>
2026-02-04 17:08:43 +0100

    feat: Create button Variants object

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,17 @@
 import type { ButtonProps } from '../../types/UiTypes';
+import type { ButtonVariants } from '../../types/UiTypes';
 
 function Button({label, type, variant}: ButtonProps){
+
+    const variants: ButtonVariants = {
+        primary: 'text-white',
+        secondary: '',
+    }
+
     return(
         <button
             type={type}
-            className={`${variant}`}
+            className={`${variants[variant]}`}
         >
             {label}
         </button>
