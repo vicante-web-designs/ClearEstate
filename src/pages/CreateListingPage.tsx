aaa532d03b0050a7e683cf15bd2bf38c3a30c5cd
Batubo Victory <victorybatubo76@gmail.com>
2026-02-05 18:36:04 +0100

    feat: Add form content

--- src/pages/CreateListingPage.tsx
+++ src/pages/CreateListingPage.tsx
@@ -1,7 +1,79 @@
+import Button from '../components/ui/Button';
+
 function CreateListingPage(){
     return(
         <form>
-            
+            <article>
+                <h2 className='text-2xl font-bold'>
+                    List Your Property with Ease
+                </h2>
+
+                <p>
+                    Fill out the form below to share your property with thousands of potential clients.
+                </p>
+            </article>
+
+            <article>
+                <label htmlFor="">
+                    Title
+                </label>
+                <input type="text" />
+            </article>
+
+            <article>
+                <label htmlFor="">
+                    Description
+                </label>
+                <input type="text" />
+            </article>
+
+            <article>
+                <label htmlFor="">
+                    Location
+                </label>
+                <input type="text" />
+            </article>
+
+            <article>
+                <label htmlFor="">
+                    Agent ID
+                </label>
+                <input type="text" />
+            </article>
+
+             <article>
+                <label htmlFor="">
+                    Price
+                </label>
+                <input type="text" />
+            </article>
+
+             <article>
+                <label htmlFor="">
+                    Tags
+                </label>
+                <input type="text" />
+            </article>
+
+             <article>
+                <label htmlFor="">
+                    Bedrooms
+                </label>
+                <input type="text" />
+            </article>
+
+             <article>
+                <label htmlFor="">
+                    Type
+                </label>
+                <input type="text" />
+            </article>
+
+            <Button 
+                label='Publish Listing'
+                variant='primary'
+                type='submit'
+            />
         </form>
     )
 }
