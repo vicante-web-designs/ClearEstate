111e0607dee5c951a291348f3e3d9d0192f44587
Batubo Victory <victorybatubo76@gmail.com>
2026-02-07 17:19:13 +0100

    feat: Wrap app in listingContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CreateListingPage from './pages/CreateListingPage'
+import { ListingContext } from './context/context'
+import { useState } from 'react'
 
 function App() {
+  const [listings, setListings] = useState('help');
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/createListings' element={<CreateListingPage />} />
-      </Routes>
-    </Router>
+      <ListingContext.Provider value={listings}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/createListings' element={<CreateListingPage />} />
+          </Routes>
+        </Router>
+      </ListingContext.Provider>
   )
 }
 
