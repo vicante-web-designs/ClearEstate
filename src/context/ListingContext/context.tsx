af9c19714992f52927a20b8223dbab3689d2f69b
Batubo Victory <victorybatubo76@gmail.com>
2026-02-09 14:56:08 +0100

    refactor: Use mock listing data

--- src/context/ListingContext/context.tsx
+++ src/context/ListingContext/context.tsx
@@ -1,11 +1,11 @@
-import { type ReactNode } from 'react';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
-import type { Listing } from '../../types/UiTypes';
+import { useState, type ReactNode } from 'react';
+import type { Listing } from '../../types/Listing';
 import { ListingContext } from './createListingContext';
+import { mockListings } from '../../data/ListingData';
 
 export function ListingProvider({ children }: { children: ReactNode }){
     
-    const [listings, setListings] = useLocalStorage<Listing[]>('listings', []);
+    const [listings, setListings] = useState<Listing[]>(mockListings);
     
     const listingValue = {
         listings,
