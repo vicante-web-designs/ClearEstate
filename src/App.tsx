f42bf5b7a9da2f78c05ac518b497575ca51e3c63
Batubo Victory <victorybatubo76@gmail.com>
2026-02-13 11:28:57 +0100

    fix: Update imports for createListingsPgae

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import CreateListingPage from './pages/CreateListingPage'
+import CreateListingPage from './pages/listings/CreateListingPage'
 import { ListingProvider } from './context/ListingContext/context'
 
 function App() {
