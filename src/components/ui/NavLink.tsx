da81e9b8be85c3802e7ea6fcb9e0e4973095b2ab
Batubo Victory <victorybatubo76@gmail.com>
2026-02-23 11:46:47 +0100

    style: Reduce navlink font sizes

--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -2,7 +2,7 @@ import type { NavLinkProps } from '../../types/UiTypes';
 
 function NavLink ({link, label}: NavLinkProps){
     return(
-        <a href={link} className={`border-b-2 border-b-blue-100 hover:border-b-2 px-2 transition-all hover:border-blue-500 ease-in duration-300`}>
+        <a href={link} className={`border-b-2 border-b-blue-100 hover:border-b-2 px-2 transition-all hover:border-blue-500 ease-in duration-300 text-sm`}>
             {label}
         </a>
     )
