6fb2dd5a045f2d39bdf6c38e47beb1138a9eb6ad
Batubo Victory <victorybatubo76@gmail.com>
2026-02-25 11:49:31 +0100

    feat: Add submit Button

--- src/components/features/SearchBar.tsx
+++ src/components/features/SearchBar.tsx
@@ -1,10 +1,20 @@
+import Button from '../ui/Button'
+
 function SearchBar(){
     return (
-        <form>
-            <label>
-                Search Listing
-                <input type="text" />
+        <form className=' mx-30'>
+            <label className='flex flex-col gap-16'>
+                <h3>
+                    Search Listing
+                </h3>
+                <input type="text" className='rounded-full w-full p-3 bg-white shadow-lg'/>
             </label>
+
+            <Button
+                label='Search'
+                variant='primary'
+                type='submit'
+            />
         </form>
     )
 }
