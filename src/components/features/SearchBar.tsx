a5268158aa541cb97c2f0677db9fbc75e4452d40
Batubo Victory <victorybatubo76@gmail.com>
2026-02-25 12:07:18 +0100

    test: Ensure search functionality

--- src/components/features/SearchBar.tsx
+++ src/components/features/SearchBar.tsx
@@ -1,21 +1,52 @@
+import { ListingContext } from '../../context/ListingContext/createListingContext'
 import Button from '../ui/Button'
+import { useContext, useState } from 'react'
 
 function SearchBar(){
-    return (
-        <form className=' mx-30'>
-            <label className='flex flex-col gap-16'>
-                <h3>
-                    Search Listing
-                </h3>
-                <input type="text" className='rounded-full w-full p-3 bg-white shadow-lg'/>
-            </label>
+    const [search, setSearch] = useState<string>('')
+    const context = useContext(ListingContext)
 
-            <Button
-                label='Search'
-                variant='primary'
-                type='submit'
-            />
-        </form>
+    if (!context) return;
+
+    const { listings } = context;
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        const newListings = listings.filter(listing => listing.title.includes(search))
+
+        console.log(newListings)
+
+        setSearch('')
+
+    }
+
+    return (
+        <div>
+            <form className=' mx-30' onSubmit={(e) => {
+                handleSubmit(e)
+            }}>
+                <label className='flex flex-col gap-16'>
+                    <h3>
+                        Search Listing
+                    </h3>
+                    <input
+                        type="text"
+                        className='rounded-full w-full p-3 bg-white shadow-lg'
+                        onChange={(e) => {
+                            setSearch(e.target.value)
+                        }}
+                        value={search}
+                    />
+                </label>
+
+                <Button
+                    label='Search'
+                    variant='primary'
+                    type='submit'
+                />
+            </form>
+        </div>
     )
 }
 
