40541ef216edcbc1bb33f8238dfbb73a0182a6c4
Batubo Victory <victorybatubo76@gmail.com>
2026-02-26 14:36:53 +0100

    style: Add full details and content on each listing detail route

--- src/pages/listings/ListingDetails.tsx
+++ src/pages/listings/ListingDetails.tsx
@@ -3,7 +3,9 @@ import { useContext } from 'react';
 import { ListingContext } from '../../context/ListingContext/createListingContext';
 import type { Listing } from '../../types/Listing';
 import { formatPrice } from '../../utils/formatPrice';
-import { MapPin, Bed, Bath, Maximize } from 'lucide-react'
+import { MapPin, Bed, Bath, Maximize, Home } from 'lucide-react'
+import Navbar from '../../layouts/Navbar';
+import Footer from '../../layouts/Footer';
 
 const ListingDetails = () => {
     const { listingId } = useParams();
@@ -19,66 +21,117 @@ const ListingDetails = () => {
     const listing = listings.find((listing: Listing) => listing.id === listingId)
 
   return (
-    listing ? (
-        <section className='max-w-3xl mx-auto'>
-            <img src={listing.images[0]} alt={`Image of ${listing.title}`} />
+    <section className='flex flex-col'>
+        <Navbar />
 
-            <div className='flex w-full justify-between items-end'>
-                <h1 className='text-5xl'>
-                    {listing.title}
-                </h1>
+        {
+            listing ? (
+                <section className='max-w-3xl mx-auto'>
 
-                <h3 className='text-green-700 font-bold'>
-                    {formatPrice(listing.price)}
-                </h3>
-            </div>
+                    {/* Listing Image */}
+                    <img src={listing.images[0]} alt={`Image of ${listing.title}`} />
 
-            <p className='text-xl max-w-[50%] my-16'>
-                {listing.description}
-            </p>
+                    {/* Listing content */}
+                    <div className='p-16'>
+                        <div className='flex w-full justify-between items-end'>
+                            <h1 className='text-5xl'>
+                                {listing.title}
+                            </h1>
 
-            <div className='flex gap-4'>
-                <MapPin color='#ff6666' size={24}/>
+                            <h3 className='text-green-700 font-bold'>
+                                {formatPrice(listing.price)}
+                            </h3>
+                        </div>
+
+                        <div className='flex justify-between items-center my-16'>
+                            {/* Listing description */}
+                            <p className='text-xl max-w-[50%]'>
+                                {listing.description}
+                            </p>
+
+                            <div className={`${listing.status.toLowerCase() === 'for sale' ? 'text-[#FF5733]' : 'text-[#20C997]'} w-fit rounded-lg bg-white shadow-lg font-bold`}>
+                                <p className='py-8 px-16'>
+                                    {listing.status}
+                                </p>
+                            </div>
+                        </div>
+
+                        {/* Listing location */}
+                        <div className='flex gap-4'>
+                            <MapPin color='#ff6666' size={24}/>
+                            <p>
+                                {`${listing.location}, ${listing.city}, ${listing.state}`}
+                            </p>
+                        </div>
+
+                        <div className='flex justify-between my-32'>
+                            <article className='w-fit'>
+                                <div className='flex justify-between'>
+                                    <Home color='gray' size={24}/>
+                                    <p className='font-bold'>
+                                        {listing.propertyType}
+                                    </p>
+                                </div>
+                                <p>Property Type</p>
+                            </article>
+
+                            <article className='w-fit'>
+                                <div className='flex justify-between'>
+                                    <Bed color='gray' size={24}/>
+                                    <p className='font-bold'>
+                                        {listing.bedrooms}
+                                    </p>
+                                </div>
+                                <p>Bedrooms</p>
+                            </article>
+
+                            <article className='w-fit'>
+                                <div className='flex justify-between'>
+                                    <Bath color='gray' size={24}/>
+                                    <p className='font-bold'>
+                                        {listing.bathrooms}
+                                    </p>
+                                </div>
+                                <p>Bathrooms</p>
+                            </article>
+
+                            <article className='w-fit'>
+                                <div className='flex justify-between'>
+                                    <Maximize color='gray' size={24}/>
+                                    <p className='font-bold'>
+                                        {`${listing.sizeSqft}`}
+                                    </p>
+                                </div>
+                                <p>Square Feet</p>
+                            </article>
+                        </div>
+
+                        <div className='flex flex-col gap-4'>
+                            <p className='font-bold'>Features:</p>
+
+                            <div className='flex w-fit gap-16'>
+                            {
+                                listing.features.map(feature => (
+                                    <p className='border px-16 py-8 rounded-lg border-secondary bg-neutral-200'>
+                                        {feature}
+                                    </p>
+                                ))
+                            }
+                        </div>
+                        </div>
+                    </div>
+
+                    
+                </section>
+            ) : (
                 <p>
-                    {`${listing.location}, ${listing.city}, ${listing.state}`}
+                    Listing does not exist
                 </p>
-            </div>
+            )
+        }
 
-            <div className='flex justify-between my-32'>
-                <article className='w-fit'>
-                    <div className='flex justify-between'>
-                        <Bed color='gray' size={24}/>
-                        <p className='font-bold'>
-                            {listing.bedrooms}
-                        </p>
-                    </div>
-                    <p>Bedrooms</p>
-                </article>
-
-                <article className='w-fit'>
-                    <div className='flex justify-between'>
-                        <Bath color='gray' size={24}/>
-                        <p className='font-bold'>
-                            {listing.bathrooms}
-                        </p>
-                    </div>
-                    <p>Bathrooms</p>
-                </article>
-
-                <article className='w-fit'>
-                    <div className='flex justify-between'>
-                        <Maximize color='gray' size={24}/>
-                        <p className='font-bold'>
-                            {`${listing.sizeSqft}`}
-                        </p>
-                    </div>
-                    <p>Square Feet</p>
-                </article>
-            </div>
-
-            
-        </section>
-    ) : ''
+        <Footer />
+    </section>
   )
 }
 
