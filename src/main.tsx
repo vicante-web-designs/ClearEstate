55a150fb24d3c1cfec6b492a509bb5264a3cb0fe
Batubo Victory <victorybatubo76@gmail.com>
2026-02-26 13:00:36 +0100

    refactor: Use latest react routing methods

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage.tsx';
+import { ListingProvider } from './context/ListingContext/context.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ListingProvider>
+      <RouterProvider router={router}/>
+    </ListingProvider>
   </StrictMode>,
 )
