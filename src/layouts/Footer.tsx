8059fd0fd75e416ff0e76a63a6a200aa35f2c218
Batubo Victory <victorybatubo76@gmail.com>
2026-02-27 14:19:51 +0100

    style: Add margin to top of footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         return date.getFullYear();
     }
     return(
-        <footer className='flex justify-center bg-primary text-white p-24'>
+        <footer className='flex justify-center bg-primary text-white p-24 mt-40'>
             <p>
                 &copy; {getCurrentYear()}  ClearListing - All rights reserved
             </p>
