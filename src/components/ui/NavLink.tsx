5ff110a636abb37a712b1b911082fba3b0ba0d98
Batubo Victory <victorybatubo76@gmail.com>
2026-03-04 17:49:55 +0100

    style: Add luxury vibe to NavLinks

--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -3,8 +3,10 @@ import { Link, type LinkProps  } from 'react-router-dom'
 
 const NavLink = ({ to, children }: LinkProps) => {
   return (
-    <Link to={to} className='text-white hover:underline hover:decoration-blue-600 transition-all ease-in duration-300 w-fit'>
-        {children}
+    <Link to={to} className='text-white hover:underline hover:underline-offset-2 hover:decoration-blue-600 transition-all ease-in duration-300 w-fit'>
+        <p className='font-light text-2xs tracking-widest uppercase'>
+          {children}
+        </p>
     </Link>
   )
 }
