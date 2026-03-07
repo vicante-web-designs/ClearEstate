2c0dd22a8bbdde256fbc0103d999dde49821f861
Batubo Victory <victorybatubo76@gmail.com>
2026-03-07 13:40:37 +0100

    fix: Add seperate listingDisplay to control listings

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,18 @@
 import HeroSection from '../layouts/HeroSection'
 import ListingCard from '../components/features/ListingCard'
-// import SearchPanel from '../components/features/SearchPanel'
+import SearchPanel from '../components/features/SearchPanel'
 import ListingLoadingState from '../components/ui/LoadingStates/ListingLoadingState'
 import { Analytics } from '@vercel/analytics/react'
 import PageLink from '../components/ui/PageLink'
 import { useSelector } from 'react-redux'
 import type { RootState } from '../state/store'
+import { useState } from 'react'
+import type { Listing } from '../types/Listing'
 
 const HomePage = () => {
     const listings = useSelector((state: RootState) => state.listings.listingValue)
+
+    const [listingDisplay, setListingDisplay] = useState<Listing[]>(listings) // For controlling the display of listings
 
 
     return(
@@ -16,15 +20,15 @@ const HomePage = () => {
             
             <HeroSection />
 
-            {/* <SearchPanel 
+            <SearchPanel 
                 filterFunction={setListingDisplay}
-            /> */}
+            />
 
             <section className='flex flex-col gap-48 items-center'>
                 <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 w-full max-w-7xl mx-auto px-6 place-items-center py-16'>
                     {
-                        listings ? (
-                            listings.map(listing => (
+                        listingDisplay.length !== 0 ? (
+                            listingDisplay.map(listing => (
                             <ListingCard 
                                 key={listing.id}
                                 id={listing.id}
--- src/pages/listings/ListingsPage.tsx
+++ src/pages/listings/ListingsPage.tsx
@@ -1,23 +1,27 @@
 import ListingCard from '../../components/features/ListingCard'
 import ListingLoadingState from '../../components/ui/LoadingStates/ListingLoadingState'
-// import SearchPanel from '../../components/features/SearchPanel'
+import SearchPanel from '../../components/features/SearchPanel'
 import PageLink from '../../components/ui/PageLink'
 import { useSelector } from 'react-redux'
 import type { RootState } from '../../state/store'
+import { useState } from 'react'
+import type { Listing } from '../../types/Listing'
 
 const ListingsPage = () => {
-    const listings = useSelector((state: RootState) => state.listings.listingValue)
+    const listings = useSelector((state: RootState) => state.listings.listingValue);
+
+    const [listingDisplay, setListingDisplay] = useState<Listing[]>(listings) // For controlling the display of listings
 
   return (
     <section className='mt-42 flex flex-col gap-48 items-center'>
-        {/* <SearchPanel 
+        <SearchPanel 
             filterFunction={setListingDisplay}
-        /> */}
+        />
 
         <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full max-w-7xl mx-auto px-6 place-items-center py-16'>
             {
-                listings.length !== 0 ? (
-                    listings.map(listing => (
+                listingDisplay.length !== 0 ? (
+                    listingDisplay.map(listing => (
                     <ListingCard 
                         key={listing.id}
                         id={listing.id}
